d532b3e584ed9e3f48308d48c444d4eae4e8066c
Yu-Ren, Zhang <e8035669@gmail.com>
2018-12-29 11:13:03 +0800

    oh! I know that whats in lmdb now!

--- util/learning_caffe02/main.cpp
+++ util/learning_caffe02/main.cpp
@@ -1,17 +1,48 @@
 #include <iostream>
+#include <memory>
 #include <caffe/caffe.hpp>
+#include <caffe/util/db.hpp>
+#include <caffe/util/io.hpp>
+#include <caffe/util/format.hpp>
 
 using namespace std;
 using namespace caffe;
 
 int main(int argc, char** argv) {
+    FLAGS_logtostderr = 1;
     google::InitGoogleLogging(argv[0]);
-    Caffe::set_mode(Caffe::Brew::CPU);
+    google::InstallFailureSignalHandler();
+    LOG(INFO) << "This project will list all data in lmdb";
 
+    unique_ptr<db::DB> db(db::GetDB("lmdb"));
+    db->Open(argv[1], db::READ);
+    unique_ptr<db::Cursor> cursor(db->NewCursor());
 
-
-    cout << "Hello World" << endl;
-
+    Datum datum;
+    while (cursor->valid()) {
+        cout << "------------------------------" << endl;
+        cout << cursor->key() << ": " << cursor->value().size() << " bytes." << endl;
+        datum.ParseFromString(cursor->value());
+        if (datum.has_channels()) {
+            cout << "Channel: " << datum.channels() << endl;
+        }
+        if (datum.has_height()) {
+            cout << "Height: " << datum.height() << endl;
+        }
+        if (datum.has_width()) {
+            cout << "Width: " << datum.width() << endl;
+        }
+        if (datum.has_data()) {
+            cout << "Data: " << datum.data().size() << " bytes." << endl;
+        }
+        if (datum.has_label()) {
+            cout << "Label: " << datum.label() << endl;
+        }
+        if (datum.has_encoded()) {
+            cout << "Encoded: " << boolalpha << datum.encoded() << endl;
+        }
+        cursor->Next();
+    }
 
     return 0;
 }
