5d6e2e84c00ee325b6d45b583dfa61fcbbffaf45
Yu-Ren, Zhang <e8035669@gmail.com>
2019-01-01 09:46:36 +0800

    Well, I can create a lmdb, I must use this lmdb for training

---
+++ lib/iris_dataset.h
@@ -0,0 +1,9 @@
+#ifndef IRIS_DATASET_H
+#define IRIS_DATASET_H
+
+extern float IRIS_DATASET[];
+
+extern int IRIS_TRUTH[];
+
+
+#endif
---
+++ util/learning_caffe04/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <memory>
+#include <caffe/caffe.hpp>
+#include <caffe/util/db.hpp>
+#include <caffe/util/io.hpp>
+#include <caffe/util/format.hpp>
+#include "iris_dataset.h"
+
+using namespace std;
+using namespace caffe;
+
+int main(int argc, char** argv) {
+    cout << "Hello world." << endl;
+    unique_ptr<db::DB> db(db::GetDB("lmdb"));
+    db->Open("iris-lmdb", db::NEW);
+    unique_ptr<db::Transaction> txn(db->NewTransaction());
+
+    Datum datum;
+    datum.set_channels(1);
+    datum.set_width(4);
+    datum.set_height(1);
+    for (int i = 0; i < 150; i++) {
+        datum.clear_label();
+        datum.clear_float_data();
+
+        for (int j = 0; j < 4; j++) {
+            datum.add_float_data(IRIS_DATASET[4 * i + j]);
+        }
+        datum.set_label(IRIS_TRUTH[i]);
+        string val;
+        datum.SerializeToString(&val);
+        string key = caffe::format_int(i, 4);
+        txn->Put(key, val);
+    }
+    txn->Commit();
+
+    return 0;
+}
